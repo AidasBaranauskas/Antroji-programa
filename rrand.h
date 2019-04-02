814ea52552f9eb4c58b9aac83e69ba7de7a6be90
AidasBaranauskas <44964790+AidasBaranauskas@users.noreply.github.com>
2019-04-02 12:31:58 +0300

    Delete rrand.h

--- rrand.h
+++
@@ -1,7 +0,0 @@
-std::random_device rd;
-std::mt19937 mt(rd());//zymiai greiciau, kuomet jie globalus
-int rrand(int a, int b)
-{
-    std::uniform_int_distribution<int> distribution(a,b);
-    return distribution(mt);
-}
